002e55c9cc5848e0a382f9a33e647f6c27a23b40
BinhNguyen <ntbinh200394@gmail.com>
2022-09-05 13:24:36 +0700

    execrise sign in form

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App';
 import StudentList from './component/studentList';
 import ProfileCard from './component/profileCard';
+import SignForm from './component/signForm';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<ProfileCard />)
+// root.render(<ProfileCard />)
+root.render(<SignForm />)
